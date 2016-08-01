78db7dd5236456fce48f9710901fcb951c742e99
unknown <apatwari@ITDV016.tdc.com>
2016-08-01 18:22:15 +0300

    adding bannerpopup functionality

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,9 @@
 import { Component, ViewChild } from '@angular/core';
-import { ionicBootstrap, Platform, Nav } from 'ionic-angular';
+import { ionicBootstrap, Platform, Nav, Modal } from 'ionic-angular';
 import { StatusBar } from 'ionic-native';
 
 import { Home } from './pages/home/home';
+import { BannerPopupPage } from './pages/banner-popup/banner-popup';
 import { Page2 } from './pages/page2/page2';
 
 @Component({
@@ -11,35 +12,45 @@ import { Page2 } from './pages/page2/page2';
 class TlbHybrid {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = Home;
+  rootPage: any = BannerPopupPage;
 
   pages: Array<{title: string, component: any, icon: string}>;
 
   constructor(private platform: Platform) {
     this.initializeApp();
-
+    let self = this;
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: Home, icon: 'icon_home.png' },
       { title: 'Page dos', component: Page2, icon: '' }
     ];
 
+    setTimeout(function() {      
+      self.nav.setRoot(Home);
+    }, 5000);
+
   }
 
   initializeApp() {
+    var self = this;
     this.platform.ready().then(() => {
-      this.platform.resume.subscribe(() => {
-          alert('I will show ');
-      });
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+
+      this.platform.resume.subscribe(() => {
+          this.nav.push(BannerPopupPage);
+          setTimeout(function() {
+            self.nav.pop();
+          }, 5000);
+      });
+
       if(this.platform.is('ios')){
         StatusBar.overlaysWebView(false);
       }
       StatusBar.backgroundColorByHexString('#7a5230');
         
-      
     });
+
   }
 
   openPage(page) {
--- app/pages/banner-popup/banner-popup.ts
+++ app/pages/banner-popup/banner-popup.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { ViewController, NavController } from 'ionic-angular';
 
 /*
   Generated class for the BannerPopupPage page.
@@ -12,8 +12,12 @@ import { NavController } from 'ionic-angular';
 })
 export class BannerPopupPage {
 
-  constructor(private nav: NavController) {
+  constructor(private nav: NavController, private view: ViewController) {
 
+  }
+
+  close(){
+      this.view.dismiss();
   }
 
 }
