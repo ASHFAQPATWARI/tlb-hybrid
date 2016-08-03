3ccc282a5a4e67d3c2a5b28d02376022a1fc8b9f
unknown <apatwari@ITDV016.tdc.com>
2016-08-03 17:12:53 +0300

    search component added

---
+++ app/components/search-restaurants/search-restaurants.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { ViewController, Modal, NavController } from 'ionic-angular';
+import { SelectionPopup } from './../../modals/selectionPopup/selection-popup';
+
+@Component({
+    selector: 'search-section',
+    templateUrl: 'build/components/search-restaurants/search-restaurants.html',
+})
+export class searchRestaurants {
+
+  constructor(private nav: NavController, private view: ViewController) {
+      
+  }
+
+  showalert = () => {
+    let modal = Modal.create(SelectionPopup);
+    modal.onDismiss(data => {
+     console.log(data);
+   });
+   this.nav.present(modal);
+  }
+
+}
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, Modal, ViewController } from 'ionic-angular';
 
-import { SelectionPopup } from './../../modals/selectionPopup/selection-popup';
-
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
@@ -11,8 +9,4 @@ export class Home {
   constructor(private navCtrl: NavController, private view: ViewController) {
   }
 
-  showalert = () => {
-    let modal = Modal.create(SelectionPopup);
-    this.navCtrl.present(modal);
-  }
 }
